f255c928f3c9002d7479dd372de35491df00d7d6
khushal0944 <khushalsuthar0944@gmail.com>
2025-05-01 13:51:30 +0530

    fixed auto deletion

--- backend/models/boardModel.js
+++ backend/models/boardModel.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose')
 
 const boardSchema = mongoose.Schema({
-    shortId: {
-        type: String,
-        required: [true, "Board id must be present"],
-        unique: true
-    },
-    content: {
-        type: String,
-        required: [true, "No Content Present"]
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        expires: 3600
-    }
-})
+	shortId: {
+		type: String,
+		required: [true, "Board id must be present"],
+		unique: true,
+	},
+	content: {
+		type: String,
+		required: [true, "No Content Present"],
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		index: { expires: 3600 }
+	},
+});
 
 const boardModel = mongoose.model("Board", boardSchema)
 
